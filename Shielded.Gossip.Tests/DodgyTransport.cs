0f1fae9480a9952d9cf21a04462446f5845fcbb1
Josip Bakic <josip.bakic@gmail.com>
2018-12-09 16:19:18 +0100

    Tests with bad transport now also delay messages
    The DodgyBackend will now also occasionally delay
    a message and deliver it up to 5 seconds later than
    expected. Slows the dodgy tests down, but they're still
    green.

--- Shielded.Gossip.Tests/DodgyTransport.cs
+++ Shielded.Gossip.Tests/DodgyTransport.cs
@@ -2,32 +2,48 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Shielded.Gossip.Tests
 {
     public class DodgyTransport : ITransport
     {
         private readonly TcpTransport _wrapped;
-        private readonly double _risk;
+        private readonly double _lossRisk;
+        private readonly double _delayRisk;
+        private readonly int _delayMaxMs;
 
-        public DodgyTransport(TcpTransport wrapped, double risk = 0.1)
+        public DodgyTransport(TcpTransport wrapped, double lossRisk = 0.1, double delayRisk = 0.1, int delayMaxMs = 5000)
         {
             _wrapped = wrapped;
-            _risk = risk;
+            _lossRisk = lossRisk;
+            _delayRisk = delayRisk;
+            _delayMaxMs = delayMaxMs;
             _wrapped.MessageReceived += _wrapped_MessageReceived;
             _wrapped.Error += _wrapped_Error;
         }
 
-        private bool ThrowDice()
+        private bool ShouldLoseMsg()
         {
             var rnd = new Random();
-            return rnd.NextDouble() < _risk;
+            return rnd.NextDouble() < _lossRisk;
         }
 
-        private void _wrapped_MessageReceived(object sender, object msg)
+        private int? GetMsgDelay()
         {
-            if (ThrowDice())
+            var rnd = new Random();
+            if (rnd.NextDouble() >= _delayRisk)
+                return null;
+            return rnd.Next(_delayMaxMs);
+        }
+
+        private async void _wrapped_MessageReceived(object sender, object msg)
+        {
+            if (ShouldLoseMsg())
                 return;
+            var delay = GetMsgDelay();
+            if (delay != null)
+                await Task.Delay(delay.Value);
             MessageReceived?.Invoke(this, msg);
         }
 
